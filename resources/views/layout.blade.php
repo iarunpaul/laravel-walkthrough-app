8ccfbf78c9bf502dcd8eed1ea53380a835475a6d
iarunpaul <iarunpaul@yahoo.com>
2020-07-05 00:50:41 +0530

    Compiled the scss and js if any using npm dependencies

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,7 @@ Released   : 20140225
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
     <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
     <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
 
 </head>
 <div id="header-wrapper">
@@ -41,4 +42,7 @@ Released   : 20140225
 <body>
     @yield ('content')
 </body>
+<div id="copyright" class="container">
+        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+    </div>
 </html>
